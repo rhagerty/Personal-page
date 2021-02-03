d36eeaaa6c5f7b741db8aa0ce90022c10b9090b9
Rebecca Hagerty <rebecca.hagerty12@gmail.com>
2021-02-02 20:17:31 -0500

    updates

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from flask import Flask, request, render_template, jsonify, session
 from boggle import Boggle
-import requests
-from pprint import pprint
-from models import GameTile
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "nevertell"
