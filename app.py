f4c234f5321a9224fdfd0beb38dc978ddeba7246
Rebecca Hagerty <rebecca.hagerty12@gmail.com>
2021-02-08 23:09:35 -0500

    updates

--- app.py
+++ app.py
@@ -12,20 +12,24 @@ def landing_page():
     """Show landing page."""
  
     return render_template("home.html")
-  
 
+@app.route("/display-resume")
+def display_resume():
+    """Show resume."""
 
-@app.route("/main")
-def homepage():
-    """Show homepage."""
+    return render_template("resume.html")
 
-    return render_template("home.html")
+@app.route("/display-about")
+def display_about():
+    """Show about me section."""
 
+    return render_template("about.html")
 
 @app.route("/display-portfolio")
 def display_portfolio():
-    return render_template("home.html", scrollToAnchor='portfolio')
+    """Show portfolio section."""
 
+    return render_template("portfolio.html")
 
 # Boggle Routes
 @app.route("/display-boggle")
--- static/ui.js
+++ static/ui.js
@@ -1,55 +1,10 @@
 $(".nextBtn").click(function () {
-  $(".header").hide();
-  $(".second").hide();
+  $(".header").slideToggle();
+  $(".second").slideToggle();
   $(".portfolio-box").show();
   $(".resume-container").hide();
   $(".nextBtn").hide();
   $(".upBtn").show();
-});
-$(".upBtn").click(function () {
-  $(".header").show();
-  $(".second").show();
-  $(".resume-container").hide();
-  $(".portfolio-box").hide();
-  $(".jeopardy-container").hide();
-  $(".connect4-container").hide();
-  $(".boggle-container").hide();
-  $(".nextBtn").show();
-  $(".upBtn").hide();
-});
-
-$(".resume").click(function () {
-  $(".portfolio-box").hide();
-  $(".header").hide();
-  $(".second").hide();
-  $(".resume-container").show();
-  $(".about-container").hide();
-  $(".nextBtn").hide();
-  $(".upBtn").show();
-});
-
-$(".next-page").click(function () {
-  $(".header").hide();
-  $(".second").hide();
-  $(".portfolio-box").show();
-  $(".resume-container").hide();
-  $(".nextBtn").hide();
-  $(".upBtn").show();
-});
-
-$(".about").click(function () {
-  $(".portfolio-box").hide();
-  $(".resume-container").hide();
-  $(".header").hide();
-  $(".second").hide();
-  $(".about-container").show();
-  $(".nextBtn").hide();
-  $(".upBtn").show();
-});
-
-$(".boggle-img").click(function () {
-  $(".portfolio-box").slideUp();
-  $(".resume-container").hide();
 });
 
 
