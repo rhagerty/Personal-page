b751c52cc8602794a3719f88cbfb3d90386bbd83
Rebecca Hagerty <rebecca.hagerty12@gmail.com>
2021-02-10 00:05:34 -0500

    updates for mobile

--- static/ui.js
+++ static/ui.js
@@ -1,10 +1,14 @@
-$(".nextBtn").click(function () {
-  $(".header").slideToggle();
-  $(".second").slideToggle();
-  $(".portfolio-box").show();
-  $(".resume-container").hide();
-  $(".nextBtn").hide();
-  $(".upBtn").show();
-});
 
 
+const mediaQuery = window.matchMedia("( max-width: 700px )");
+
+if (mediaQuery.matches) {
+  $(".img").attr("src", "/static/images/me.png");
+} else {
+  $(".img").attr("src", "/static/images/me.jpg");
+}
+
+mediaQuery.addEventListener(handleTabletChange);
+
+// Initial check
+handleTabletChange(mediaQuery);
